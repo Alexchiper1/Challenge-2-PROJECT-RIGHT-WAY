a79800359be1035dfe6443f3b53a8d299425280b
Alexchiper1 <B00161706@mytudublin.ie>
2024-11-14 15:30:12 +0000

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldown;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1.0f;
+        }
+        if(cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+}
